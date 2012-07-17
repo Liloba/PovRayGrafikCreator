cc8be45d873ac56bd85ff355d366388727f89bc8
spritly <britaleitner@gmx.de>
2012-07-17 09:27:01 +0200

    projekt

---
+++ sourcecode/PovRayGraficCreator/PovRayGraficCreator/PovRayGraficCreator.cpp
@@ -0,0 +1,18 @@
+// PovRayGraficCreator.cpp: Hauptprojektdatei.
+
+#include "stdafx.h"
+#include "fMain.h"
+
+using namespace PovRayGraficCreator;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Aktivieren visueller Effekte von Windows XP, bevor Steuerelemente erstellt werden
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Hauptfenster erstellen und ausführen
+	Application::Run(gcnew Form1());
+	return 0;
+}
