ca8dcfc5848aaaf2eb23371bf16c94954319c773
spritly <britaleitner@gmx.de>
2012-07-18 14:20:55 +0200

    erste figuren klassen (unvollstaendig)

--- sourcecode/PovRayGraficCreator/gitObjects.cpp
+++ sourcecode/PovRayGraficCreator/gitObjects.cpp
@@ -1,1 +1,32 @@
 #include "gitObjects.h"
+
+#define SPHERE_STR		"Kugel"
+#define BOX_STR			"Box"
+#define CYLINDER_STR	"Cylinder"
+
+// Kugel implementieren
+void TPr_Sphere::Init(){
+	mName = SPHERE_STR;
+	mRadius = 0;
+}
+void TPr_Sphere::DeInit(){	
+}
+
+// Quader
+void TPr_Box::Init(){
+	mName = BOX_STR;
+	mLocation2 = new TLocation();
+}
+void TPr_Box::DeInit(){
+	delete mLocation2;
+}
+
+// Zylinder
+void TPr_Cylinder::Init(){
+	mName = CYLINDER_STR;
+	mRadius = 0;
+	mLocation2 = new TLocation;
+}
+void TPr_Cylinder::DeInit(){
+	delete mLocation2;
+}
--- sourcecode/PovRayGraficCreator/gitObjects.h
+++ sourcecode/PovRayGraficCreator/gitObjects.h
@@ -7,5 +7,82 @@
 #ifndef GITOBJECTS_H
 #define GITOBJECTS_H
 
+#include <string>
 
+
+using namespace std;
+
+class TLocation{
+public:
+	int mX, mY, mZ;
+	TLocation(){
+		mX = mY = mZ = 0;		
+	}
+};
+
+// allgemeines PovRay Object
+class TPr_Object abstract{
+public:
+	TLocation *mLocation;
+//	Bitmap mThumbnail;
+	string mName;
+//	??? mColor;
+//	??? mStructure;
+private:
+	virtual void Init() = 0;
+	virtual void DeInit() = 0;
+public:
+	TPr_Object(){ 
+		Init();
+	};
+	~TPr_Object(){
+		DeInit();
+	}
+	TLocation getLocation(){ 
+		return *mLocation; 
+	}
+	string GetName(){
+		return  mName;
+	}
+};
+
+// Kugel
+class TPr_Sphere:public TPr_Object{
+private:
+	int mRadius;
+	void Init();
+	void DeInit();
+public:
+	int GetRadius(){
+		return mRadius;
+	}
+};
+
+// Quader
+class TPr_Box:public TPr_Object{
+private:
+	TLocation *mLocation2;
+	void Init();
+	void DeInit();
+public:
+	TLocation GetLocation2(){
+		return *mLocation2;
+	}
+};
+
+// Zylinder
+class TPr_Cylinder:public TPr_Object{
+private:
+	TLocation *mLocation2;
+	int mRadius;
+	void Init();
+	void DeInit();
+public:
+	TLocation GetLocation2(){
+		return *mLocation2;
+	}
+	int GetRadius(){
+		return mRadius;
+	}
+};
 #endif
